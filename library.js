4e9d8a7cf6efff36b5edf68a2aeec7bc14d30986
Victor Zamanian <victor.zamanian@gmail.com>
2013-02-13 22:04:29 +0100

    Can now basically use as independent audio player.

--- library.js
+++ library.js
@@ -1,12 +1,37 @@
+var currentDir = "/";
+var playQueue = [];
+
 window.onload = function() {
-	loadLibraryDirectory("library", function(response) {
-		var json = JSON.parse(response);
-		var artists = document.getElementById("artists");
-		for (var i = 0; i < json.length; i++) {
-			var item = document.createElement("li");
-			item.textContent = json[i];
-			artists.appendChild(item);
+	audio = document.getElementById("audio");
+	dirButton = document.getElementById("dirbutton");
+	dirField = document.getElementById("dirfield");
+	fileList = document.getElementById("filelist");
+	audio.addEventListener('ended', function() {
+		console.log('Playback has ended for song: ' + currentSong.textContent);
+		currentSong = currentSong.nextElementSibling;
+		this.src = "library/" + currentDir + "/" + currentSong.textContent;
+		console.log('Now playing: ' + currentSong.textContent);
+		this.play();
+	});
+
+	dirButton.addEventListener('click', function() {
+		currentDir = "/" + dirField.value;
+		while (fileList.childElementCount) {
+			fileList.removeChild(fileList.firstChild);
 		}
+		loadLibraryDirectory(currentDir, function(response) {
+			var json = JSON.parse(response);
+			for (var i = 0; i < json.length; i++) {
+				var listItem = document.createElement("li");
+				listItem.textContent = json[i];
+				fileList.appendChild(listItem);
+				listItem.addEventListener('click', function() {
+					audio.src = "library/" + currentDir + "/" + this.textContent;
+					currentSong = this;
+					audio.play();
+				});
+			}
+		});
 	});
 };
 
@@ -20,5 +45,7 @@ function loadLibraryDirectory(dir, callback) {
 		};
 		httpRequest.open("GET", "cgi-bin/library.py?dir=" + dir, true);
 		httpRequest.send(null);
+	} else {
+		alert("Your browser does not support XMLHttpRequest. (Try Chrome?)");
 	}
 }
