1105112494be897876ce4775502bbad0e08de8b5
Victor Zamanian <victor.zamanian@gmail.com>
2013-04-15 14:48:48 +0200

    Sort both directories and files.
    Also, sort the final, culled lists. Not the raw lists which might
    contain entries we don't need, so it's unnecessary to sort those.

--- cgi-bin/library.py
+++ cgi-bin/library.py
@@ -10,9 +10,9 @@ form = cgi.FieldStorage()
 print("Content-type: text/html\n")
 if "dir" in form:
 	basedir = "../library/" + form["dir"].value
-	dirlist = sorted(os.listdir(basedir))
+	dirlist = os.listdir(basedir)
 	directories = [d for d in dirlist if os.path.isdir(basedir + "/" + d)]
 	#files = [f for f in dirlist if not os.path.isdir(basedir + "/" + f)]
 	files = [os.path.basename(f) for f in glob.glob(basedir + "/*.mp3")]
-	jsonData = {"directories": directories, "files": files}
+	jsonData = {"directories": sorted(directories), "files": sorted(files)}
 	print(json.dumps(jsonData))
