33a37f3301a399267858e7d297f6e42be4f2f11f
Victor Zamanian <victor.zamanian@gmail.com>
2013-05-21 01:37:34 +0200

    Rewrote code from scratch to open dirs inline.
    All of the code has not been written yet; of the playback controls have
    not been hooked up to event listeners yet and continual play has not
    been implemented.

--- library.js
+++ library.js
@@ -1,175 +1,3 @@
-const baseDir = "library/";
-var currentDir = "/";
-var currentTrack;
-//var playQueue = [];
-
-window.onload = function() {
-	audio = document.getElementById("audio");
-	var volume = document.getElementById("volume-bar");
-	volume.addEventListener("change", function () {
-		audio.volume = volume.value;
-	});
-	dirField = document.getElementById("dirfield");
-	prevButton = document.getElementById("prev");
-	playButton = document.getElementById("play");
-	nextButton = document.getElementById("next");
-	seekSlider = document.getElementById("seek");
-	playButton.addEventListener("click", function() {
-		if (!currentTrack) {
-			currentTrack = document.querySelector(".librarylist .file");
-			if (currentTrack) {
-				setNewTrackAsPlaying(currentTrack);
-			}
-		} else if (audio.paused) {
-			audio.play();
-		} else {
-			audio.pause();
-		}
-	});
-	prevButton.addEventListener("click", previousTrack);
-	nextButton.addEventListener("click", nextTrack);
-
-	audio.addEventListener("ended", nextTrack);
-
-	audio.addEventListener("play", function() {
-		playButton.textContent = "Pause";
-	});
-
-	audio.addEventListener("pause", function() {
-		playButton.textContent = "Play";
-	});
-
-	audio.addEventListener("timeupdate", function() {
-		seekSlider.value = audio.currentTime / audio.duration;
-	});
-
-	seekSlider.addEventListener("mousedown", function() {
-		if (!audio.paused) {
-			audio.pause();
-			audio.wasJustPlaying = true;
-		}
-	});
-
-	seekSlider.addEventListener("mouseup", function() {
-		if (audio.wasJustPlaying) {
-			audio.currentTime = seekSlider.value * (audio.duration ? audio.duration : 0);
-			audio.play();
-			audio.wasJustPlaying = false;
-		}
-	});
-
-	// Add keyboard shortcuts.
-	window.addEventListener('keydown', function(e) {
-		switch (e.keyCode) {
-			case 75: // 'k'
-				if (audio.paused) {
-					audio.play();
-				} else {
-					audio.pause();
-				}
-				break;
-			case 74: // 'j'
-				audio.currentTime -= 10;
-				break;
-			case 76: // 'l'
-				audio.currentTime += 10;
-				break;
-			case 38: // up arrow
-				if (audio.volume > 0.95) {
-					audio.volume = 1;
-				} else {
-					audio.volume += 0.05;
-				}
-				volume.value = audio.volume;
-				break;
-			case 40: // down arrow
-				if (audio.volume < 0.05) {
-					audio.volume = 0;
-				} else {
-					audio.volume -= 0.05;
-				}
-				volume.value = audio.volume;
-				break;
-		}
-	});
-
-	// load library on page load
-	loadLibraryDirectory("/", parseJSON, {"dir": "/"});
-};
-
-function nextTrack() {
-	if (currentTrack) {
-		setNewTrackAsPlaying(currentTrack.nextElementSibling);
-	}
-}
-
-function previousTrack() {
-	if (currentTrack) {
-		setNewTrackAsPlaying(currentTrack.previousElementSibling);
-	}
-}
-
-function setNewTrackAsPlaying(newTrack) {
-	if (currentTrack) {
-		currentTrack.classList.remove("playing");
-	}
-	if (newTrack && newTrack.classList.contains("file")) {
-		newTrack.classList.add("playing");
-		audio.src = baseDir + currentDir + "/" + newTrack.textContent;
-		console.log("Now playing: " + newTrack.textContent);
-		currentTrack = newTrack;
-		audio.play();
-	}
-}
-
-function parseJSON(json, callbackArgs) {
-	var parsedJSON = JSON.parse(json);
-	if (parsedJSON.status !== "OK") {
-		alert("Error: " + parsedJSON.error);
-		return;
-	}
-	currentDir = callbackArgs.dir;
-	var entryList = document.getElementById("librarylist");
-	while (entryList.childElementCount) {
-		entryList.removeChild(entryList.firstChild);
-	}
-	// add parent directory list entry if not at the root directory
-	if (currentDir !== "/") {
-		var listItem = document.createElement("li");
-		listItem.textContent = "..";
-		listItem.classList.add("directory");
-		entryList.appendChild(listItem);
-		listItem.addEventListener("click", function() {
-			// open directory
-			var dir = currentDir.slice(0, currentDir.lastIndexOf("/"));
-			if (dir === "") {
-				dir = "/";
-			}
-			loadLibraryDirectory(dir, parseJSON, {"dir": dir});
-		});
-	}
-	for (var i = 0; i < parsedJSON.directories.length; i++) {
-		var listItem = document.createElement("li");
-		listItem.textContent = parsedJSON.directories[i];
-		listItem.classList.add("directory");
-		entryList.appendChild(listItem);
-		listItem.addEventListener("click", function() {
-			// open directory
-			var dir = currentDir + "/" + this.textContent;
-			loadLibraryDirectory(dir, parseJSON, {"dir": dir});
-		});
-	}
-	for (var i = 0; i < parsedJSON.files.length; i++) {
-		var listItem = document.createElement("li");
-		listItem.textContent = parsedJSON.files[i];
-		listItem.classList.add("file");
-		entryList.appendChild(listItem);
-		listItem.addEventListener("click", function() {
-			setNewTrackAsPlaying(this);
-		});
-	}
-}
-
 function loadLibraryDirectory(dir, callback, callbackArgs) {
 	if (window.XMLHttpRequest) {
 		var httpRequest = new XMLHttpRequest();
@@ -178,9 +6,96 @@ function loadLibraryDirectory(dir, callback, callbackArgs) {
 				callback(httpRequest.response, callbackArgs);
 			}
 		};
-		httpRequest.open("GET", "cgi-bin/library.py?dir=" + dir, true);
+		httpRequest.open("GET", "cgi-bin/library.py?dir=" + encodeURIComponent(dir), true);
 		httpRequest.send(null);
 	} else {
-		alert("Your browser does not support XMLHttpRequest. (Try Chrome?)");
+		alert("Your browser does not support XMLHttpRequest, which is required to use this web application.");
 	}
 }
+
+function listElementsFromArray(array) {
+	var elements = null;
+	if (array && array.length) {
+		elements = new Array(array.length);
+		for (var i = 0; i < array.length; i++) {
+			var li = document.createElement("li");
+			li.textContent = array[i];
+			elements[i] = li;
+		}
+	}
+	return elements;
+}
+
+function populateList(list, elements) {
+	for (var i = 0; i < elements.length; i++) {
+		list.appendChild(elements[i]);
+	}
+}
+
+function addEventListeners(list, callback, eventType) {
+	for (var i = 0; i < list.length; i++) {
+		list[i].addEventListener(eventType, callback);
+	}
+}
+
+function closeDir(dir) {
+	dir.removeChild(dir.querySelector("ul.librarylist"));
+}
+
+function openDir(dir) {
+	var newList = document.createElement("ul");
+	var path = dir.parentNode.getAttribute("data-path") + "/" + dir.textContent;
+	newList.setAttribute("data-path", path);
+	newList.classList.add("librarylist");
+	dir.appendChild(newList);
+
+	var callbackArgs = { list: newList };
+	loadLibraryDirectory(path, handleJSONData, callbackArgs);
+}
+
+function directoryClicked(e) {
+	if (e.target.classList.contains("open")) {
+		e.target.classList.remove("open");
+		closeDir(e.target);
+	} else {
+		e.target.classList.add("open");
+		openDir(e.target);
+	}
+	e.stopPropagation();
+}
+
+function fileClicked(e) {
+	playFile(e.target.parentNode.getAttribute("data-path") + "/" + e.target.textContent)
+	e.stopPropagation();
+}
+
+function handleJSONData(data, args) {
+	var json = JSON.parse(data);
+	if (json.status !== "OK") {
+		alert("unable to load library listing");
+		return;
+	}
+
+	var directories = listElementsFromArray(json.directories);
+	var files = listElementsFromArray(json.files);
+	if (directories && directories.length) {
+		addEventListeners(directories, directoryClicked, "click");
+		populateList(args.list, directories);
+	}
+	if (files && files.length) {
+		addEventListeners(files, fileClicked, "click");
+		populateList(args.list, files);
+	}
+}
+
+function playFile(path) {
+	audio.src = "library" + path;
+	audio.play();
+}
+
+window.addEventListener("load", function() {
+	audio = document.getElementById("audio");
+	var libraryList = document.getElementById("librarylist");
+	var callbackArgs = { list: libraryList };
+	loadLibraryDirectory("/", handleJSONData, callbackArgs);
+});
